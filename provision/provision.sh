0f4cd8639d51853238524dc8a7363fce036c13dd
Chan <cglee.toqo@gmail.com>
2018-12-09 12:12:33 -0600

    adds bash to core tools

--- provision/provision.sh
+++ provision/provision.sh
@@ -4,7 +4,7 @@ yum -y update
 # Add the CentOS 7 EPEL repository
 yum -y install epel-release
 # Install core tools
-yum -y install make gcc-g++ git vim curl
+yum -y install make gcc-g++ git vim curl bash
 
 /vagrant/provision/install-nginx.sh
 /vagrant/provision/install-node.sh
